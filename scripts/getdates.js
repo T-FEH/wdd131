2ea48de4222b480943904a7d48ed0e4cd5afb366
T-FEH <alewibolu05@gmail.com>
2025-11-02 14:19:27 +0100

    Add initial project files including HTML, CSS, JavaScript, and images

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Get the current year and set it in the footer
+document.getElementById("currentyear").innerHTML = new Date().getFullYear();
+
+// Get the last modified date and set it in the footer
+document.getElementById("lastModified").innerHTML = "Last modified: " + document.lastModified;
